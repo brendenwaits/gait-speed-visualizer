9a4d12ee019d449a313815ee72200fb35235939c
Brenden Waits <brendenwaits@gatech.edu>
2021-04-19 15:08:51 -0400

    added dummy data script

---
+++ dummy.py
@@ -0,0 +1,19 @@
+'''
+This script posts dummy data values to the api for testing
+'''
+
+import requests
+import random
+import json
+
+from datetime import datetime, timedelta
+
+random.seed(datetime.now())
+url = "http://localhost:8000/api/gaitmeasurements/"
+
+for i in range(100):
+    speed = round(random.uniform(.7, 1.5), 2)
+    date = (datetime.now() - timedelta(days=i)).isoformat()
+
+    x = requests.post(url, data={"speed": speed, "date": date})
+    print(x)
